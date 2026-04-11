7e3f2397782eb89f6be22de6691e8d4164c4d3a2
sarathprabhav <sarathprabhav@gmail.com>
2026-04-11 19:24:59 +0200

    new website first commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+// Highlight active navbar link automatically
+const links = document.querySelectorAll('.nav-link');
+const current = window.location.pathname.split('/').pop();
+
+links.forEach(link => {
+  if(link.getAttribute('href') === current) {
+    link.classList.add('active');
+  }
+});
+
+
+function convert() {
+  let ly = document.getElementById("ly").value;
+  let km = ly * 9.461e12;
+  document.getElementById("result").innerText =
+    ly + " light years = " + km + " km";
+}
+
